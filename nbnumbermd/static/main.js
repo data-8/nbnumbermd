84e5d8271f76eae4584725f565b77b3c32ecd1e7
Jacky Lu <jackylu@berkeley.edu>
2017-06-27 12:58:05 -0700

    Added initial files

---
+++ nbnumbermd/static/main.js
@@ -0,0 +1,12 @@
+define([
+  'base/js/namespace',
+  'jquery',
+  'require',
+  'base/js/events',
+  'base/js/utils',
+  'base/js/dialog'
+], function(Jupyter, $, require, events, utils, dialog) {
+  'use strict';
+
+  
+});
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="nbnumbermd",
+    version='0.1.0',
+    author="Jacky Lu",
+    description="",
+    packages= ["nbnumbermd"]
+    install_requires=[
+        'notebook',
+    ],
+    package_data={'nbnumbermd': ['static/*']},
+)
